29b21bab619b22e2f4cc0d558760aa8947b12ffe
Chris Benz <me@chrisbenz.dev>
2024-10-02 18:55:57 -0300

    Build script updates for host architecture support

--- scripts/local-build.sh
+++ scripts/local-build.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# Copyright 2023 Balena.io
+# Modifications Copyright 2024 Sortile
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#
+# Modifications:
+# - local-build.sh now builds for the host architecture instead of cross-compiling
+
 set -ev
 
 if [ -z "$1" ]; then
-	printf 'Rust compilation target not specified'
+	printf 'Rust compilation target not specified\n'
 	exit 1
 fi
 
 TARGET=$1
-ARCH=$2
 
-cross() {
-    docker run -it --rm -v $PWD:/work majorz/rust-$ARCH:rust-1.23.0 "$@"
-}
+# Build the project
+cargo build --release --target=$TARGET
 
-cross cargo build --release --target=$TARGET
-
-cross cross-strip target/$TARGET/release/wifi-connect
+# Strip the binary to reduce its size
+strip target/$TARGET/release/wifi-connect
